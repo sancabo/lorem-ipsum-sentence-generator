5dd5ad0c003490f74af17b5a4ba29cb109b9698c
Sabtiago Cabo <newsancabo@gmail.com>
2023-01-02 01:04:19 -0300

    initial commit

---
+++ src/main/java/com/devsancabo/www/App.java
@@ -0,0 +1,9 @@
+package com.devsancabo.www;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(LoremIpsum.getWords(50,8));
+        //System.out.println(LoremIpsum.getWords(3523523, 3523523));
+    }
+}
---
+++ src/main/java/com/devsancabo/www/LoremIpsum.java
@@ -0,0 +1,123 @@
+package com.devsancabo.www;
+
+public final class LoremIpsum {
+    private static final String PARAGRAPH = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
+    private static final String[] LOREM_IPSUM_WORDS = PARAGRAPH.split("\\s");
+    private static final int WORDS_ARRAY_LENGTH = 50;
+
+    private LoremIpsum(){}
+    /**
+     * Get a sentence of 20 words from the lorem ipsum text.
+     * @return a String containing the sentence.
+     */
+    public static String getWords() {return getWords(20, 0);
+    }
+
+    /**
+     * Get a sentence of an arbitrary amount of words from the lorem ipsum text.
+     * The words will start to loop around after 50.
+     * @param amount he amount of words to generate.
+     * @return a String containing the words, separated by space.
+     */
+    public static String getWords(int amount) {
+        return fasterGetWords(amount);
+    }
+
+    /**
+     * Get a sentence of an arbitrary amount of words from the lorem ipsum text.
+     * The words will start to loop around after 50.
+     * The first word will be determined by an offset. This selector will also loop after 50.
+     * @param amount The amount of words to generate.
+     * @param offset How many words to skip before selecting the first one.
+     * @return a String containing the words separated by space.
+     */
+    public static String getWords(int amount, int offset) {
+        if (amount <= 0) return "";
+        StringBuilder lorem = new StringBuilder();
+        int word = offset % WORDS_ARRAY_LENGTH;
+
+        String firstWord = LOREM_IPSUM_WORDS[word % WORDS_ARRAY_LENGTH];
+        String casedFirstWord = firstWord.substring(0,1).toUpperCase() + firstWord.substring(1);
+        lorem.append(casedFirstWord);
+        lorem.append(' ');
+
+        for(int i = word + 1; i < word + amount; ++i) {
+            lorem.append(LOREM_IPSUM_WORDS[i % WORDS_ARRAY_LENGTH]);
+            lorem.append(' ');
+        }
+        return fixEnd(lorem.toString().stripTrailing());
+    }
+
+    private static String fasterGetWords(int amount) {
+        if (amount <= 0) return "";
+        //add chunks of 50 words instead of 1
+        int loops = amount / WORDS_ARRAY_LENGTH;
+        int endIndex = amount - (loops * WORDS_ARRAY_LENGTH);
+        StringBuilder lorem = new StringBuilder();
+        for(int i = 0; i < loops; ++i) {
+            lorem.append(PARAGRAPH);
+            lorem.append(' ');
+        }
+        for(int i = 0; i < endIndex; ++i) {
+            lorem.append(LOREM_IPSUM_WORDS[i]);
+            lorem.append(' ');
+        }
+        return fixEnd(lorem.toString().stripTrailing());
+    }
+
+    private static String fixEnd(String rawResult){
+        if(rawResult.charAt(rawResult.length() - 1) == ',')
+            return rawResult.substring(0,rawResult.lastIndexOf(',')) + ".";
+        if(rawResult.charAt(rawResult.length() - 1) != '.')
+            return rawResult + ".";
+        return rawResult;
+    }
+
+    /**
+     * Get two paragraphs of lorem ipsum words.
+     * Each paragraph's content is equivalent to LoremIpsum.getWords(50).
+     * @return a String containing the paragraphs, separated by two newlines.
+     */
+    public static String getParagraphs() {
+        return getParagraphs(2);
+    }
+
+    /**
+     * Get an arbitrary amount of paragraphs of lorem ipsum words.
+     * Each paragraph's content is equivalent to LoremIpsum.getWords(50).
+     * @param amount The amount of paragraphs to generate.
+     * @return a String containing the paragraphs separated by two newlines.
+     */
+    public static String getParagraphs(int amount) {
+        StringBuilder lorem = new StringBuilder();
+
+        for(int i = 0; i < amount; ++i) {
+            lorem.append(PARAGRAPH);
+            if (i < amount - 1) {
+                lorem.append("\n\n");
+            }
+        }
+
+        return lorem.toString();
+    }
+
+    /**
+     * Get an arbitrary amount of paragraphs of lorem ipsum words.
+     * Each paragraph's content is equivalent to LoremIpsum.getWords(paragraphLength).
+     * @param amount The amount of paragraphs to generate.
+     * @param paragraphLength The length of each generated paragraph.
+     * @return a String containing the paragraphs separated by two newlines.
+     */
+    public static String getParagraphs(int amount, int paragraphLength) {
+        StringBuilder lorem = new StringBuilder();
+
+        for(int i = 0; i < amount; ++i) {
+            lorem.append(getWords(paragraphLength));
+            if (i < amount - 1) {
+                lorem.append("\n\n");
+            }
+        }
+
+        return lorem.toString();
+    }
+}
